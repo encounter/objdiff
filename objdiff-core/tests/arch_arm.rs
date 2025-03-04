9c31c82a37c20a2ca3628542119568f203c0fe4e
Luke Street <luke@street.dev>
2025-03-03 21:08:36 -0700

    cargo fmt

--- objdiff-core/tests/arch_arm.rs
+++ objdiff-core/tests/arch_arm.rs
@@ -8,7 +8,8 @@ fn read_arm() {
     let diff_config = diff::DiffObjConfig { mips_register_prefix: true, ..Default::default() };
     let obj = obj::read::parse(include_object!("data/arm/LinkStateItem.o"), &diff_config).unwrap();
     insta::assert_debug_snapshot!(obj);
-    let symbol_idx = obj.symbols.iter().position(|s| s.name == "_ZN13LinkStateItem12OnStateLeaveEi").unwrap();
+    let symbol_idx =
+        obj.symbols.iter().position(|s| s.name == "_ZN13LinkStateItem12OnStateLeaveEi").unwrap();
     let diff = diff::code::no_diff_code(&obj, symbol_idx, &diff_config).unwrap();
     insta::assert_debug_snapshot!(diff.instruction_rows);
     let output = common::display_diff(&obj, &diff, symbol_idx, &diff_config);
