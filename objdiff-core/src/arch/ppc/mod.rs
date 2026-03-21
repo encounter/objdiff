7b34b557602724d6e718b38f3b90f812dab5c829
Amber Brault <celestialamber1@gmail.com>
2026-03-21 16:11:45 +0100

    Make compiler shut up

--- objdiff-core/src/arch/ppc/mod.rs
+++ objdiff-core/src/arch/ppc/mod.rs
@@ -475,7 +475,7 @@ impl Arch for ArchPpc {
 
     fn post_init(&mut self, _sections: &[Section], _symbols: &[Symbol], symbol_indices: &[usize]) {
         // Change the indices used as keys from the original symbol indices to the new symbol array indices
-        if let Some(new_map) = Self::convert_extab_map_indices(&self, symbol_indices) {
+        if let Some(new_map) = Self::convert_extab_map_indices(self, symbol_indices) {
             self.extab.replace(new_map);
         }
     }
@@ -486,11 +486,14 @@ impl ArchPpc {
         self.extab.as_ref()?.get(&symbol_index)
     }
 
-    pub fn convert_extab_map_indices(&self, symbol_indices: &[usize]) -> Option<BTreeMap<usize, ExceptionInfo>> {
+    pub fn convert_extab_map_indices(
+        &self,
+        symbol_indices: &[usize]
+    ) -> Option<BTreeMap<usize, ExceptionInfo>> {
         let new_map: BTreeMap<usize, ExceptionInfo> =
             self.extab.as_ref()?.iter().map(|e| (symbol_indices[*e.0 + 1], e.1.clone())).collect();
 
-        return Some(new_map)
+        Some(new_map)
     }
 }
 
